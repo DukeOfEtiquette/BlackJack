fbf093d66a03028f5db3e76a6710b79ea91b5bf2
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-21 00:25:54 -0800

    fixed hand class

--- Hand.h
+++ Hand.h
@@ -1,18 +1,24 @@
+
 #ifndef Hand_cpp
 #define Hand_cpp
 
 #include "Card.h"
 #include <vector>
 
-class Hand {
+class Hand
+{
 public:
-	std::vector<Card*> m_hand;
-	int m_handID;
-
-	int SumHand();
-	void PrintHand();
-	void DumpHand();
-
+    Hand(int size);
+    std::vector<Card*> handy;
+    int SumHand();
+    void PrintHand();
+    void DumpHand();
+private:
+    int m_handID;
 };
+
+Hand::Hand(int id) {
+    m_handID = id;
+}
 
 #endif
