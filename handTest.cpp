40d948ddd891473937f70848597918c22ece45bc
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-20 23:40:28 -0800

    made a Hand class but still needs work

--- handTest.cpp
+++ handTest.cpp
@@ -2,44 +2,78 @@
 #include <vector>
 #include "Card.h"
 #include "Hand.h"
+#include "assert.h"
+
+class Hands
+{
+public:
+    Hands(int size);
+    std::vector<Card*> handy;
+    int SumHand();
+    void PrintHand();
+    void DumpHand();
+private:
+    int m_handID;
+};
+
+Hands::Hands(int id) {
+    m_handID = id;
+}
+
 
 int main()
 {
-		std::vector<Card*> hand;
-		Card* c;
-		
-		Card::Suit spade = Card::Spade;
-		Card::Suit heart = Card::Heart;
-		Card::Suit club = Card::Club;
-		Card::Suit diamond = Card::Diamond;
-
-		for (int i = 2; i < 14; i++) {
-				c = new Card(i, spade);
-				hand.push_back(c);
-		}
-
-		for (int i = 2; i < 14; i++) {
-				c = new Card(i, heart);
-				hand.push_back(c);
-		}
-		for (int i = 2; i < 14; i++) {
-				c = new Card(i, club);
-				hand.push_back(c);
-		}
-		for (int i = 2; i < 14; i++) {
-				c = new Card(i, diamond);
-				hand.push_back(c);
-		}
-		
-		for (int i = 0; i < hand.size(); i++)
-		{
-				int val;
-				std::cout << "Card: ";
-				hand[i]->PrintCard();
-				val = hand[i]->GetValue();
-				std::cout << " Value: " << val;
-				std::cout << std::endl;
-				//hand.SumHand();
-		}
+    Hands hand(1);  // handID is 1
+    
+    Card* c;
+    
+    
+    Card::Suit heart = Card::Heart;
+    Card::Suit diamond = Card::Diamond;
+    Card::Suit club = Card::Club;
+    Card::Suit spade = Card::Spade;
+    
+    for(int i = 2; i < 14; i++)
+    {
+        c = new Card(i, heart);
+        hand.handy.push_back(c);
+    }
+    
+    for(int i = 2; i < 14; i++)
+    {
+        c = new Card(i, diamond);
+        hand.handy.push_back(c);
+    }
+    for(int i = 2; i < 14; i++)
+    {
+        c = new Card(i, club);
+        hand.handy.push_back(c);
+    }
+    for(int i = 2; i < 14; i++)
+    {
+        c = new Card(i, spade);
+        hand.handy.push_back(c);
+    }
+    
+    int val;
+    
+    for(int i = 0; i < hand.handy.size(); i++)
+    {
+        std::cout << "Card: ";
+        hand.handy[i]->PrintCard();
+        val = hand.handy[i]->GetValue();
+        std::cout << " Value: " << val;
+        std::cout << std::endl;
+    }
+    val = hand.SumHand();
+    std::cout << "SumHand(): " << val << std::endl;
+    hand.PrintHand();
+    
+    hand.DumpHand();
+    assert(hand.handy[0] == 0);
+    
 }
+
+
+
 
