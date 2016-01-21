cb86ff4bd6bd026992b79d2236eb3f56426047b0
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-20 21:47:59 -0800

    hand class created

--- Hand.h
+++ Hand.h
@@ -1,11 +1,18 @@
+#ifndef Hand_cpp
+#define Hand_cpp
+
 #include "Card.h"
 #include <vector>
 
+class Hand {
 public:
 	std::vector<Card*> m_hand;
 	int m_handID;
 
-
 	int SumHand();
 	void PrintHand();
 	void DumpHand();
+
+};
+
+#endif
