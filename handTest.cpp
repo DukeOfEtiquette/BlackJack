9b4df6bab5d9719e2e86a632e84219f0234b9585
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-22 00:12:10 -0800

    added commits and test code

--- handTest.cpp
+++ handTest.cpp
@@ -9,7 +9,7 @@ int main()
 	Hand* h = new Hand(0);
 	
 	Card* c;
-
+	
     Card::Suit heart = Card::Heart;
     Card::Suit diamond = Card::Diamond;
 	Card::Suit club = Card::Club;
@@ -17,16 +17,22 @@ int main()
 
     c = new Card(1, heart);
     h->m_hand.push_back(c);
-	//c = new Card(2, club);
 	//h->m_hand.push_back(c);
 	//c = new Card(3, diamond);
 	//h->m_hand.push_back(c);
+	//c = new Card(4, club);
+	for (int i = 2; i < 14; i++) 
+	{
+		c = new Card(i, spade);
+		h->m_hand.push_back(c);
+	}
 	
-
     std::cout << "SumHand(): " << h->SumHand() << std::endl;    
     std::cout << "PrintHand(): "; 
     h->PrintHand();   
-    std::cout << std::endl;
+	std::cout << h->m_hand[0]->GetValue();
+
+	std::cout << std::endl;
  
 	h->DumpHand();   
 	assert (h->m_hand.empty());
