b4964eeab9c43fa83b1ed340012f1ccaa5a39b56
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-21 23:03:40 -0800

    added assert

--- handTest.cpp
+++ handTest.cpp
@@ -2,21 +2,26 @@
 #include <vector>
 #include "Card.h"
 #include "Hand.h"
+#include <assert.h>
 
 int main()
 {
-	std::cout << "Hello World!\n";
-
 	Hand* h = new Hand(0);
-
-	Card* c;
 	
+	Card* c;
 
     Card::Suit heart = Card::Heart;
     Card::Suit diamond = Card::Diamond;
+	Card::Suit club = Card::Club;
+	Card::Suit spade = Card::Spade; 
 
-    c = new Card(2, heart);
+    c = new Card(1, heart);
     h->m_hand.push_back(c);
+	//c = new Card(2, club);
+	//h->m_hand.push_back(c);
+	//c = new Card(3, diamond);
+	//h->m_hand.push_back(c);
+	
 
     std::cout << "SumHand(): " << h->SumHand() << std::endl;    
     std::cout << "PrintHand(): "; 
@@ -24,7 +29,8 @@ int main()
     std::cout << std::endl;
  
 	h->DumpHand();   
-          
+	assert (h->m_hand.empty());
+	
 	return 0;
 }
 
