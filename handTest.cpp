4d2b570f912c27447e687c35c18b3339724c71ad
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-21 22:43:07 -0800

    now works, still working on testing cases

--- handTest.cpp
+++ handTest.cpp
@@ -10,5 +10,22 @@ int main()
 	Hand* h = new Hand(0);
 
 	Card* c;
+	
+
+    Card::Suit heart = Card::Heart;
+    Card::Suit diamond = Card::Diamond;
+
+    c = new Card(2, heart);
+    h->m_hand.push_back(c);
+
+    std::cout << "SumHand(): " << h->SumHand() << std::endl;    
+    std::cout << "PrintHand(): "; 
+    h->PrintHand();   
+    std::cout << std::endl;
+ 
+	h->DumpHand();   
+          
 	return 0;
 }
+
+
