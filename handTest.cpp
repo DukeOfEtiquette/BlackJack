c345b837dbf9bc931f2a5d8da2f0fa542c078df0
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-22 01:47:02 -0800

    added more tests

--- handTest.cpp
+++ handTest.cpp
@@ -3,13 +3,16 @@
 #include "Card.h"
 #include "Hand.h"
 #include <assert.h>
+#include <math.h>
+#include <exception>
 
 int main()
 {
 	Hand* h = new Hand(0);
-	
-	Card* c;
-	
+	assert (h != NULL);	
+
+	Card* c;	
+
     Card::Suit heart = Card::Heart;
     Card::Suit diamond = Card::Diamond;
 	Card::Suit club = Card::Club;
@@ -17,7 +20,9 @@ int main()
 
     c = new Card(2, heart);
     h->m_hand.push_back(c);
-	
+	assert (h->m_hand[0]->GetValue() == 2);
+	assert (h->m_hand.size() > 0);	
+
 	for (int i = 2; i < 14; i++) 
 	{
 		c = new Card(i, heart);
@@ -39,11 +44,29 @@ int main()
 		h->m_hand.push_back(c);
 	}
 	
+	// test for x
+	c = new Card(400, spade); 
+	h->m_hand.push_back(c);
+	
+	assert (isnan(h->SumHand()) == false); // SumHand has a number
+	assert (h->SumHand() > 0); // SumHand > 0
+	
     std::cout << "SumHand(): " << h->SumHand() << std::endl;    
     std::cout << "PrintHand(): "; 
-    
-	h->PrintHand();   
-	std::cout << h->m_hand[0]->GetValue();
+	
+	try 
+	{
+		h->PrintHand();
+		std::cout << h->m_hand[0]->GetValue();
+		if (h->m_hand[0] == 0) 
+		{
+			throw -1;
+		}
+	}
+	catch (int e)
+	{
+		std::cout << "GetValue was unsuccessful error:"<< e << std::endl;
+	}
 
 	std::cout << std::endl;
  
