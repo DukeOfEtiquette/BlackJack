6661d22afc3c0ce7d52be0cf772e7a5fae7a43ad
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-26 12:16:15 -0800

    added comments to Ace

--- Ace.cpp
+++ Ace.cpp
@@ -4,13 +4,16 @@
 
 
 Ace::Ace(int val, Suit s) : Card(val, s)
+//Ace is dervived from Card and is used in Deck.cpp
 {
+	//Makes sure the value of Ace is 1 
 	if(val != 1)
 	{
 		std::cout << "Invalid value for Ace - " << val << std::endl;
 		return;
 	}
 
+	//Sets the member variables of Ace to their default values 
 	m_value = 11;
 	m_suit = s;
 	m_bLowVal = false;
@@ -18,6 +21,7 @@ Ace::Ace(int val, Suit s) : Card(val, s)
 
 int Ace::GetValue()
 {
+	//Gets the value of Ace, if boolean LowVal is true then the value will be 1 
 	if(m_bLowVal)
 		return 1;
 	else
@@ -26,6 +30,7 @@ int Ace::GetValue()
 
 void Ace::PrintCard()
 {
+	//Prints A and the suit of Ace
 	char val, s;
 
 	switch(m_suit)
@@ -55,6 +60,7 @@ void Ace::PrintCard()
 
 void Ace::DecValue()
 {
+	//Decrements the value of an Ace by 10 (back to 1) and sets boolean LowVal to true
 	if(!m_bLowVal)
 	{
 		m_value -= 10;
