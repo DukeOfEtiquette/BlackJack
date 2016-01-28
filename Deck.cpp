a293fd623e95ceb73bec926b264cd81bbcf446f2
Broc Nickodemus <Broc@Brocs-MacBook-Pro.local>
2016-01-27 23:12:17 -0800

    fixed deck comments

--- Deck.cpp
+++ Deck.cpp
@@ -12,7 +12,7 @@
 ***********************************************************************************/
 Deck::Deck(int sets)
 {
-	m_nSets = sets; //sets is passed in, m_nSets is used in InitializeSets
+	m_nSets = sets; 
 	InitializeSet();
 }
 
@@ -28,16 +28,15 @@ Deck::~Deck()
 /***********************************************************************************
  * Purpose: Initializes a Deck
  * Out: A Deck of cards is created with 52 cards. 
+ * The i loop will occur once for every set of cards we want to make
+ * The j loop will happen once for every suit in a set
+ * The k loop will happen for values 2 through 13, or 2 through King
 ***********************************************************************************/
 void Deck::InitializeSet()
 {
-	//Pointer to a Card and Ace to be used while creating a Deck
 	Card* c;
 	Ace* a;
 
-	//The i loop will occur once for every set of cards we want to make
-	//The j loop will happen once for every suit in a set
-	//The k loop will happen for values 2 through 13, or 2 through King
 	for(int i = 0; i < m_nSets; i++)
 	{
 		for(int j = 0; j < 4; j++)
@@ -62,10 +61,8 @@ int shuffleRandom(int i) {return std::rand()%i;}
 
 void Deck::Shuffle()
 {
-	//Seed a random time
 	std::srand(unsigned(std::time(0)));
 
-	//This will shuffle everything in the vector using our custom random	
 	std::random_shuffle(m_deck.begin(), m_deck.end(), shuffleRandom);
 }
 
@@ -76,8 +73,6 @@ void Deck::Shuffle()
 ***********************************************************************************/
 Card* Deck::DealCard()
 {
-	//Here we set the value of c to be the card at the end of the deck
-	//We then pop that card off the deck and return c
 	Card* c = m_deck.back();
 	m_deck.pop_back();
 	
